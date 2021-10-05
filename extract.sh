9f9843c822b2d171474b77be4d0d2e52efc02cc6
eranok <taras.boichuk.ki.2020@lpnu.ua>
2021-10-05 20:37:21 +0000

    fixed tests with new regex for extractor. fixed extractor

--- extract.sh
+++ extract.sh
@@ -16,5 +16,5 @@ for file in ./tests_html/*.html; do
 	output=$(echo "$output" | sed '/Текст питання/!d;s//&\n/;s/.*\n//;:a;/ asdfghjkl/bb;$!{n;ba};:b;s//\n&/;P;D')
 	output=$(echo "$output" | sed -r 's/\[.[0-9]{1,10}\]//g')
 	output=$(echo "$output" | perl -ne 'print unless /\(\*\)/ ... /Коментар/')
-	echo "$output" >> ./tests/$filename.txt
+	echo "$output" > ./tests/$filename.txt
 done
